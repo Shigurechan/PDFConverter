c21b9ddffb76dc2c17fb43a6b9a5238822eff52e
松澤賢 <yw3250118.2g2g@hotmail.com>
2021-08-31 13:16:32 +0900

    マルチスレッド化中

---
+++ src/Converter.java
@@ -0,0 +1,163 @@
+
+import java.io.IOException;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
+public class Converter extends Thread
+{
+	File fileName;	//ディレクトリパス
+	
+	//画像クラス
+	class Image
+	{
+		//コンストラクタ
+		public Image(int w,int h,String n)
+		{
+			width = w;
+			height = h;
+			path = n;
+		}
+		
+		
+		public int width;	//横
+		public int height;	//縦
+		public String path;	//パス
+	}
+	
+	//ファイルの画像パスを取得
+	private void GetDirectory(File dir,List<Image> list)
+	{
+		
+		System.out.println("==================== ファイル読み込み　==================== ");
+		
+		int i = 0;
+		for(; i < dir.listFiles().length; i++)
+		{				
+			boolean w = false;
+			try 
+			{
+				BufferedImage b  = ImageIO.read(dir.listFiles()[i]);
+				Image img = new Image(b.getWidth(),b.getHeight(),dir.listFiles()[i].getPath());
+						
+				list.add(img);
+				
+				b = null;
+			} 
+			catch(NullPointerException e)
+			{
+				
+				w = true;
+				System.out.println("###### Warning: 未対応のファイル形式です: " + dir.listFiles()[i].getPath() + " ######");
+				
+			}
+			catch (IOException e) 
+			{
+				
+				e.printStackTrace();
+			}
+			
+			if ( w == false)
+			{
+				System.out.println(list.get(list.size() - 1).path);
+			}
+			//System.out.println("###################################");
+			//System.out.println("Width: " + list.get(list.size() -1).width);
+			//System.out.println("Height: " + list.get(list.size() -1).height);
+			//System.out.println("Path: " + list.get(list.size() -1).path);
+			//System.out.println("###################################\n");
+		}	
+		
+		System.out.println("ページ数: " + i);
+		
+		
+		
+		System.out.println();
+		//System.out.println();
+	}
+	
+	
+	//PDFファイルを生成
+	private void GeneratePDF(List<Image> list,List<String> pathList,File fileName)
+	{
+		try
+		{
+			PDDocument document = new PDDocument();	//ドキュメント
+			List<PDPage> page = new ArrayList<>();	//ページ
+			
+			System.out.println("==================== ページ生成　==================== ");	//画面表示
+			
+			//ページ生成
+			for(Image image  : list)
+	    	{							
+				PDRectangle rec = new PDRectangle();
+				rec.setUpperRightX(0);
+				rec.setUpperRightY(0);
+				rec.setLowerLeftX(image.width);
+				rec.setLowerLeftY(image.height);
+				
+				System.out.println(image.path + "  --- size ---> ("+ image.width + " , " + image.height + ")");
+					
+				page.add(new PDPage(rec));
+				document.addPage(page.get(page.size() -1));
+					
+	    	}
+			
+			System.out.println("\n==================== 書き込み ====================");
+			
+			//画像焼き付け 
+			for(int i = 0; i < list.size(); i++)
+	    	{
+				
+				PDImageXObject xImage = PDImageXObject.createFromFile(list.get(i).path,document);
+				PDPageContentStream stream = new PDPageContentStream(document,page.get(i));
+				stream.drawImage(xImage, 0,0);
+					
+				System.out.println( "[成功]: "+ new File(list.get(i).path).getName());
+				stream.close();	
+	    	}
+			
+			System.out.println("==================== PDF生成中 ====================\n");
+			document.save(fileName.getParent() + "\\" + fileName.getName() + ".pdf");	
+			System.out.println("--->: " + fileName.getParent() + "\\" + fileName.getName() + ".pdf");
+			System.out.println("\n==================== 完了 ====================");
+			
+			document.close();
+						
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	//コンストラクタ
+	public Converter(String filePath) 
+	{				
+		fileName = new File(filePath);
+	}
+	
+	public void Run()
+	{
+		List<Image> imageList  = new ArrayList<>();
+		List<String> fileList = new ArrayList<>();
+			
+		System.out.println();
+		GetDirectory(fileName,imageList);					//画像パス読み込み 	
+		GeneratePDF(imageList,fileList,fileName);			//PDFファイルを生成				
+	}
+	
+	
+	
+}
--- src/Main.java
+++ src/Main.java
@@ -1,151 +1,17 @@
 
-
-import java.io.IOException;
-import java.io.File;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.List;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
-
-
 public class Main 
 {
-	//画像クラス
-	static class Image
-	{
-		//コンストラクタ
-		public Image(int w,int h,String n)
-		{
-			width = w;
-			height = h;
-			path = n;
-		}
-		
-		
-		public int width;	//横
-		public int height;	//縦
-		public String path;	//パス
-	}
-
-
-	
-	
-	//ファイルの画像パスを取得
-	static public void GetDirectory(File dir,List<Image> list)
-	{
-		for(int i = 0; i < dir.listFiles().length; i++)
-		{
-			
-			try 
-			{
-				BufferedImage b  = ImageIO.read(dir.listFiles()[i]);
-				Image img = new Image(b.getWidth(),b.getHeight(),dir.listFiles()[i].getPath());
-						
-				list.add(img);
-				
-				b = null;
-			} 
-			catch (IOException e) 
-			{
-				e.printStackTrace();
-			}
-			
-			System.out.println(list.get(list.size() - 1).path);
-			//System.out.println("###################################");
-			//System.out.println("Width: " + list.get(list.size() -1).width);
-			//System.out.println("Height: " + list.get(list.size() -1).height);
-			//System.out.println("Path: " + list.get(list.size() -1).path);
-			//System.out.println("###################################\n");
-		}	
-		
-		System.out.println();
-		System.out.println();
-	}
-	
-	//PDFファイルを生成
-	static void GeneratePDF(List<Image> list,List<String> pathList,File fileName)
-	{
-		try
-		{
-			PDDocument document = new PDDocument();	//ドキュメント
-			List<PDPage> page = new ArrayList<>();	//ページ
-			
-			for(Image image  : list)
-	    	{
-				
-					
-				PDRectangle rec = new PDRectangle();
-				rec.setUpperRightX(0);
-				rec.setUpperRightY(0);
-				rec.setLowerLeftX(image.width);
-				rec.setLowerLeftY(image.height);
-				
-				page.add(new PDPage(rec));
-				document.addPage(page.get(page.size() -1));				
-	    	}
-			
-			
-			 
-			for(int i = 0; i < list.size(); i++)
-	    	{
-				
-				PDImageXObject xImage = PDImageXObject.createFromFile(list.get(i).path,document);
-				PDPageContentStream stream = new PDPageContentStream(document,page.get(i));
-				stream.drawImage(xImage, 0,0);
-					
-				System.out.println( "[成功]: "+ new File(list.get(i).path).getName());
-				stream.close();	
-	    	}
-			
-			
-						
-			document.save(fileName.getParent() + "\\" + fileName.getName() + ".pdf");	
-			
-			System.out.println("==================== 完了 ====================");
-			System.out.println(">: " + fileName.getParent() + "\\" + fileName.getName() + ".pdf");
-			document.close();
-			
-			
-		
-			
-			
-			
-		}catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	
 	public static void main(String args[]) 
-	{
-		Scanner scanner = new Scanner(System.in);
-		List<Image> imageList  = new ArrayList<>();
-	  
-		System.out.print("Directory: ");
-		String fileName = scanner.next();
-		File f = new File(fileName);
-	  
-		GetDirectory(f,imageList);	//画像パス読み込み
-	  
-	  
-		List<String> fileList = new ArrayList<>();
-		GeneratePDF(imageList,fileList,f);	//PDFファイルを生成
-	  
-	  
-	  
-	  
-		scanner.close();
-	  
+	{		
+		Converter conv1 = new Converter("C:\\Users\\yw325\\Desktop\\resource");
+		Converter conv2 = new Converter("C:\\Users\\yw325\\Desktop\\resource - コピー (2)");
+		Converter conv3 = new Converter("C:\\Users\\yw325\\Desktop\\resource - コピー");
+		
+		conv1.start();
+		//conv2.start();
+		//conv3.start();
+		
+		System.out.println("あああ");
 	}
 }
 
